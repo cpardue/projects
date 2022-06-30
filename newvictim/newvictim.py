15f5396ce7ca19aaa63e02618e38796d879468b9
cpardue <38402024+cpardue@users.noreply.github.com>
2022-06-30 13:58:07 -0500

    made banner, fun part is over

---
+++ newvictim/newvictim.py
@@ -0,0 +1,24 @@
+################################################
+####### Written by Chris Pardue in 2022 ########
+########## https://chris-pardue.com ############
+############## Written in Python3 ##############
+################################################
+
+import os
+import sys
+import subprocess
+
+
+
+os.system("clear") # clear the terminal
+print("#########################################################")
+print("# by cpardue circa 2022                  ___            #")
+print("# github.com/cpardue/resources  |\ |    |__     |  |    #")
+print("# chris-pardue.com              | \|    |___    |/\|    #")
+print("#                                                       #")
+print("#                                    __  ___            #")
+print("#                            \  / | /  `  |  |  |\/|    #")
+print("#                             \/  | \__,  |  |  |  |    #")
+print("#                                                       #")
+print("#########################################################")
+print("\n")
