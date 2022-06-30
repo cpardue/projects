2d550654ae0493eb5d8272b4b124202857a0772c
cpardue <38402024+cpardue@users.noreply.github.com>
2022-06-30 14:17:36 -0500

    added first half of cli commands, no if/else logic

--- newvictim/newvictim.py
+++ newvictim/newvictim.py
@@ -22,3 +22,29 @@ print("#                             \/  | \__,  |  |  |  |    #")
 print("#                                                       #")
 print("#########################################################")
 print("\n")
+if not os.geteuid()==0: # check for root
+    sys.exit('This script must be run as root!')  #if not root, disclaimer text, exit
+LabMachineName = input("What is the new lab's name :")  #Ask for input: What is the new lab's name? Input = LabMachineName
+RHOST = input("What is the new lab's IP address :")  #Ask for input: What is the new lab's IP address? Input = RHOST
+
+os.system("mkdir ./"+LabMachineName+"/")  #os.system touch ./LabMachineName
+#if CTF_NOTES.ctb exists,
+os.system("cp ./CTF_NOTES.ctb ./"+LabMachineName+"/"+LabMachineName+"_Recon.ctb")  #os.system cp ./CTF_NOTES.ctb ./LabMachineName/LabMachineName_Recon.ctb
+#else wget from github/cpardue/resources
+os.system("cp ./CTF_NOTES.ctb ./"+LabMachineName+"/"+LabMachineName+"_Recon.ctb")  #os.system cp ./CTF_NOTES.ctb ./LabMachineName/LabMachineName_Recon.ctb
+#if GenericRecon.py exists,
+os.system("cp ./GenericRecon.py ./"+LabMachineName+"/")  #os.system cp ./GenericRecon.py ./LabMachineName/
+#else wget from github/cpardue/resources
+os.system("cp ./GenericRecon.py ./"+LabMachineName+"/")  #os.system cp ./GenericRecon.py ./LabMachineName/
+os.system("touch ./"+LabMachineName+"/Notes.txt")  #os.system touch ./LabMachineName/notes.txt
+os.system("mkdir ./"+LabMachineName+"/Payloads/")  #os.system touch ./LabMachineName/Payloads/
+
+#os.system ping RHOST -t 5
+#if success
+#os.system open cherrytree LabMachineName_Recon.ctb &
+
+#Ask for input: Do you want to run GenericRecon.py? y/n 
+#Input = GenericReconAnswer 
+#if GenericReconAnswer, 
+#os.system run GenericRecon.py RHOST
+#else exit
