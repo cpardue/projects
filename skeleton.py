4e4a05c960107d4c99765f9ef2c071283a2e9720
cpardue <38402024+cpardue@users.noreply.github.com>
2022-11-01 15:51:05 -0500

    Create skeleton.py

---
+++ skeleton.py
@@ -0,0 +1,19 @@
+# import STRING
+# from STRING import STRING
+
+func_a (pass)
+logger.debug('Starting func_a()...')
+logger.debug('Ending func_a()...')
+
+func_b (pass)
+logger.debug('Starting func_b()...')
+logger.debug('Ending func_b()...')
+
+func_c (pass)
+logger.debug('Starting func_c()...')
+logger.debug('Ending func_c()...')
+
+main()
+func_a
+func_b
+func_c
