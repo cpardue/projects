15b8bfdbe29539602465af9ae6b31d8634f8a479
Chris Pardue <38402024+cpardue@users.noreply.github.com>
2022-11-12 08:28:04 -0600

    edited format, added mechanism checks

--- mx_recon/mx_recon.py
+++ mx_recon/mx_recon.py
@@ -1,4 +1,4 @@
-""" version 0.11, by Chris Pardue, see chris-pardue.com
+""" version 0.1, by Chris Pardue, see chris-pardue.com
 or github.com/cpardue for more sketchy python scripts"""
 import dns.resolver
 
@@ -8,50 +8,58 @@ domain = input("Enter a domain to check: ")
 def spf_check(domain):
     try:
         test_spf = dns.resolver.resolve(domain, 'TXT')
+        print("######################")
         for spf_data in test_spf:
             if 'spf1' in str(spf_data):
-                print("SPF record found: ", spf_data)
+                print("# SPF record found:\n#\t", spf_data)
+                if "redirect=" in str(spf_data):
+                    print("# \tA Redirect was found")
+                if "include:" in str(spf_data):
+                    print("# \tAn Include was found")
                 if "+all" in str(spf_data):
-                    print("-->SPF ALL mechanism: +all (PASS)")
+                    print("# \tSPF ALL mechanism: +all (PASS)")
                 elif "?all" in str(spf_data):
-                    print("-->SPF ALL mechanism: ?all (NEUTRAL)")
+                    print("# \tSPF ALL mechanism: ?all (NEUTRAL)")
                 elif "~all" in str(spf_data):
-                    print("-->SPF ALL mechanism: ~all (SOFTFAIL)")
+                    print("# \tSPF ALL mechanism: ~all (SOFTFAIL)")
                 elif "-all" in str(spf_data):
-                    print("-->SPF ALL mechanism: -all (HARDFAIL)")
+                    print("# \tSPF ALL mechanism: -all (HARDFAIL)")
                 else:
-                    print("-->SPF ALL mechanism: unknown")
+                    print("# \tSPF ALL mechanism: unknown")
     except:
-        print("[FAIL] SPF record not found.")
+        print("# [FAIL] SPF record not found.")
         pass
 
 
 def dmarc_check(domain):
     try:
         test_dmarc = dns.resolver.resolve("_dmarc." + domain, "TXT")
+        print("######################")
         for dmarc_data in test_dmarc:
             if "DMARC1" in str(dmarc_data):
-                print("DMARC record found: ", dmarc_data)
+                print("# DMARC record found:\n#\t", dmarc_data)
                 if "p=reject" in str(dmarc_data):
-                    print("-->DMARC policy: reject")
+                    print("# \tDMARC policy: reject")
                 elif "p=quarantine" in str(dmarc_data):
-                    print("-->DMARC policy: quarantine")
+                    print("# \tDMARC policy: quarantine")
                 elif "p=none" in str(dmarc_data):
-                    print("-->DMARC policy: none")
+                    print("# \tDMARC policy: none")
                 else:
-                    print("-->DMARC policy: unknown")
+                    print("# \tDMARC policy: unknown")
 
     except:
-        print("[FAIL] DMARC record not found.")
+        print("# [FAIL] DMARC record not found.")
         pass
 
 
 try:
     test_mx = dns.resolver.resolve(domain, "MX")
+    print("######################")
+    print("# MX records found:")
     for mx_data in test_mx:
-        print("MX record found: ", mx_data)
+        print("# \t", mx_data)
     spf_check(domain)
     dmarc_check(domain)
 except:
-    print("[FAIL] MX record not found.")
+    print("# [FAIL] MX record not found.")
     pass
