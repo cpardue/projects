37662714107babcfc8ddf589a811fb5a86a8c09b
Chris Pardue <38402024+cpardue@users.noreply.github.com>
2022-11-11 18:12:47 -0600

    added dmarc and spf policy mechanisms

--- mx_recon/mx_recon.py
+++ mx_recon/mx_recon.py
@@ -1,4 +1,4 @@
-""" version 0.1, by Chris Pardue, see chris-pardue.com 
+""" version 0.11, by Chris Pardue, see chris-pardue.com
 or github.com/cpardue for more sketchy python scripts"""
 import dns.resolver
 
@@ -11,6 +11,16 @@ def spf_check(domain):
         for spf_data in test_spf:
             if 'spf1' in str(spf_data):
                 print("SPF record found: ", spf_data)
+                if "+all" in str(spf_data):
+                    print("-->SPF ALL mechanism: +all (PASS)")
+                elif "?all" in str(spf_data):
+                    print("-->SPF ALL mechanism: ?all (NEUTRAL)")
+                elif "~all" in str(spf_data):
+                    print("-->SPF ALL mechanism: ~all (SOFTFAIL)")
+                elif "-all" in str(spf_data):
+                    print("-->SPF ALL mechanism: -all (HARDFAIL)")
+                else:
+                    print("-->SPF ALL mechanism: unknown")
     except:
         print("[FAIL] SPF record not found.")
         pass
@@ -19,9 +29,18 @@ def spf_check(domain):
 def dmarc_check(domain):
     try:
         test_dmarc = dns.resolver.resolve("_dmarc." + domain, "TXT")
-        for dns_data in test_dmarc:
-            if "DMARC1" in str(dns_data):
-                print("DMARC record found: ", dns_data)
+        for dmarc_data in test_dmarc:
+            if "DMARC1" in str(dmarc_data):
+                print("DMARC record found: ", dmarc_data)
+                if "p=reject" in str(dmarc_data):
+                    print("-->DMARC policy: reject")
+                elif "p=quarantine" in str(dmarc_data):
+                    print("-->DMARC policy: quarantine")
+                elif "p=none" in str(dmarc_data):
+                    print("-->DMARC policy: none")
+                else:
+                    print("-->DMARC policy: unknown")
+
     except:
         print("[FAIL] DMARC record not found.")
         pass
